f88f456988890f52ecec01cad6b795babea7b3d5
Florencia Rocca <florenciarocca@live.com>
2022-04-05 09:32:06 -0300

    hubo un error en merge

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
- 
-
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main (void) {
-    cout << "========= Inicio del programa ========" << endl;
-    cout << "Hola" << endl;
-    cout << "Chau!" << endl;
-
-    return 0;
+    fstream newfile;
+    newfile.open("text_file.txt",ios::in); //open a file to perform read operation using file object
+    if (newfile.is_open())
+    { //checking whether the file is open
+        string tp;
+        while(getline(newfile, tp)){ //read data from file object and put it into string.
+        cout << tp << "\n"; //print the data of the string
+    }
+    newfile.close(); //close the file object.
+    }
 }
 
 /*
