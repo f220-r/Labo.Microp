8042c418310c28836d6881039c6d974769cc128e
Florencia Rocca <florenciarocca@live.com>
2022-05-30 22:31:10 -0300

    cleaning

--- Ejercicios/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "matematica.h"
-
-
-int main(void) {
-	cout << "Probando multip inline: " << Multiplicar_Sat(2, 2) << endl;
-	cout << "Direcc variable global: " << &c_a_count << endl;
-    return 0;
-}
